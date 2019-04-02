7889dd02b0cdeeb487f84754d3440a92397e8bd4
mcouste <couste.mathias@gmail.com>
2019-04-02 21:53:28 +0200

    export more

--- projects/ngleaf/src/lib/components/pages/public-api.ts
+++ projects/ngleaf/src/lib/components/pages/public-api.ts
@@ -2,4 +2,9 @@
  * Public API Surface of ngleaf
  */
 
+export * from './account-settings/account-settings-avatar/account-settings-avatar.component';
+export * from './account-settings/account-settings-general/account-settings-general.component';
+export * from './account-settings/account-settings-password/account-settings-password.component';
+export * from './account-settings/account-settings.component';
+
 export * from './LeafComponentPages.module';
