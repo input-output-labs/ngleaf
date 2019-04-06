134a0bcb3f26c4644a7cee3cb7fc17ac307e3fca
NablaT <remi.pourtier@gmail.com>
2019-04-06 11:55:57 +0200

    Updating navigation component with content as input

--- projects/ngleaf/src/lib/components/templates/navigation/navigation.component.ts
+++ projects/ngleaf/src/lib/components/templates/navigation/navigation.component.ts
@@ -1,7 +1,4 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Component, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'leaf-navigation',
@@ -10,11 +7,9 @@ import { map } from 'rxjs/operators';
 })
 export class NavigationComponent {
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
+  @Input() contentTemplate: TemplateRef<any>;
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor() {
+  }
 
 }
