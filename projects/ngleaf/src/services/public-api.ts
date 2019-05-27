74f1fca024ab99a3af808948185147f9f4819612
mcouste <couste.mathias@gmail.com>
2019-05-27 21:45:42 +0200

    Export LeafAuthHttpClient

--- projects/ngleaf/src/services/public-api.ts
+++ projects/ngleaf/src/services/public-api.ts
@@ -3,5 +3,6 @@ export * from './leaf-admin.service';
 export * from './leaf-notification.service';
 export * from './leaf-session.service';
 export * from './leaf-upload-file.service';
+export * from './leaf-auth-http-client.service';
 
 export * from './leaf-config.module';
