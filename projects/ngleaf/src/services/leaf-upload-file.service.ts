a6187309d0bd707d0bef69a4fc4bf6c8946b2d3e
mcouste <couste.mathias@gmail.com>
2020-03-05 19:23:06 +0100

    Add event when uploading file

--- projects/ngleaf/src/services/leaf-upload-file.service.ts
+++ projects/ngleaf/src/services/leaf-upload-file.service.ts
@@ -1,14 +1,15 @@
 import { Injectable, Inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { LeafAuthHttpClient } from './leaf-auth-http-client.service';
-import { map } from 'rxjs/operators';
 import { LeafFileModel } from '../models/leaf-file.model';
 import { LeafConfigServiceToken } from './leaf-config.module';
+import { LeafNotificationService } from './leaf-notification.service';
 
 @Injectable()
 export class LeafUploadFileService {
 
   constructor(
+    public notificationService: LeafNotificationService,
     @Inject(LeafConfigServiceToken) private config,
     private http: LeafAuthHttpClient) {}
 
@@ -17,10 +18,34 @@ export class LeafUploadFileService {
 
     formdata.append('file', file);
 
-    return this.http
-      .post(this.config.serverUrl + '/files', formdata, {
-        reportProgress: true,
-      })
-      .pipe(map((body: LeafFileModel) => body.url));
+    this.notificationService.emit({
+      id: 'successChangeUsername',
+      category: 'session',
+      message: 'Name changed'
+    });
+
+    const url$ = new Subject<string>();
+
+    this.http.post(this.config.serverUrl + '/files', formdata, { reportProgress: true }).subscribe(
+      (uploadedFile: LeafFileModel) => {
+        this.notificationService.emit({
+          id: 'successUploadFile',
+          category: 'file-upload',
+          message: 'File successfully uploaded'
+        });
+        url$.next(uploadedFile.url);
+      },
+      () => {
+        this.notificationService.emit({
+          id: 'failureUploadFile',
+          category: 'file-upload',
+          message: 'Error during file upload'
+        });
+        url$.next(null);
+      }
+    );
+
+
+    return url$;
   }
 }
