20443976a9bae0abe732ad9301de09127cbaa72a
mathiascouste <couste.mathias@gmail.com>
2022-09-12 18:07:03 +0200

    Add date range picker to statistics viewer

--- projects/ngleaf/src/components/statistics/statistics-viewer/statistics-viewer.component.ts
+++ projects/ngleaf/src/components/statistics/statistics-viewer/statistics-viewer.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-import { filter, map, Observable } from 'rxjs';
+import { combineLatest, distinctUntilChanged, filter, map, Observable, ReplaySubject, startWith, Subject } from 'rxjs';
 import { selectStatisticsData } from '../../../store/index';
 import { LeafStatistic } from '../../../api/models/index';
 import { ChartSerie } from './chart.model';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'leaf-statistics-viewer',
@@ -15,29 +16,67 @@ export class StatisticsViewerComponent {
   @Input()
   public key: string;
 
+  @Input()
+  public showDatePicker: boolean = true;
+
+  @Input()
+  public datePickerPosition: 'topRight' | 'topLeft' | 'bottomRight' | 'bottomLeft' = 'topRight';
+
   public series$: Observable<ChartSerie[]>;
 
+  public dateRange$: Observable<{start: Date | undefined, end: Date | undefined}>;
+
+  public dateRangeFormGroup: FormGroup;
+
   constructor(private store: Store) {
-    this.series$ = this.store.pipe(
-      select(selectStatisticsData),
-      filter((statistics: LeafStatistic[]) => !!statistics),
-      map((statistics: LeafStatistic[]) => statistics.map(
-        (statistic: LeafStatistic) => ({
-          date: statistic.creationDateTime,
-          data: statistic.data[this.key]
-        })
-      )),
-      map((data: {date: Date, data: number}[]) => [{
-        name: this.key,
-        series: data.map((dat) => ({
-          name: this.dateToString(dat.date),
-          value: dat.data
-        }))
-      }])
+    this.dateRangeFormGroup = new FormGroup({
+      start: new FormControl<Date | null>(null),
+      end: new FormControl<Date | null>(null),
+    });
+
+    this.dateRange$ = this.dateRangeFormGroup.valueChanges;
+
+    this.clearDateRange();
+    this.series$ = combineLatest([
+      this.store.pipe(
+        select(selectStatisticsData),
+        filter((statistics: LeafStatistic[]) => !!statistics),
+        map((statistics: LeafStatistic[]) => statistics.map(
+          (statistic: LeafStatistic) => ({
+            date: statistic.creationDateTime,
+            data: statistic.data[this.key]
+          })
+        ))
+      ),
+      this.dateRange$.pipe(startWith({start: undefined, end: undefined}), distinctUntilChanged())
+    ]).pipe(
+      map(([data, range]) => {
+        const filteredData = data.filter(dat => !!dat.data).filter(data => {
+          if (range.start && new Date(data.date).getTime() < range.start.getTime()) {
+            return false;
+          }
+          if (range.end && new Date(data.date).getTime() > range.end.getTime()) {
+            return false;
+          }
+          return true;
+        });
+        return [{
+          name: this.key,
+          series: filteredData.map((dat) => ({
+            name: this.dateToString(dat.date),
+            value: dat.data
+          }))
+        }]
+      })
     );
   }
 
   private dateToString(date: Date) {
     return date.toString();
+  }
+
+  public clearDateRange() {
+    this.dateRangeFormGroup.controls.start.setValue(undefined);
+    this.dateRangeFormGroup.controls.end.setValue(undefined);
   }
 }
--- projects/ngleaf/src/components/statistics/statistics-viewer/statistics-viewer.module.ts
+++ projects/ngleaf/src/components/statistics/statistics-viewer/statistics-viewer.module.ts
@@ -2,10 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatisticsViewerComponent } from './statistics-viewer.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   imports: [
+    /* Core deps */
     CommonModule,
+    ReactiveFormsModule,
+    /* Mat deps */
+    MatButtonModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    /* Other deps */
     NgxChartsModule
   ],
   declarations: [StatisticsViewerComponent],
