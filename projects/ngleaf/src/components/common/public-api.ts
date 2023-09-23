0fff56e6e181175bc51b81a25497ea16e26dcba5
NablaT <remi.pourtier@gmail.com>
2023-09-23 20:32:27 +0200

    ngleaf 2.8.5: copy to clipboard

--- projects/ngleaf/src/components/common/public-api.ts
+++ projects/ngleaf/src/components/common/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of ngleaf
  */
 export * from './confirm-dialog/index';
+export * from './copy-to-clipboard/index';
 export * from './forbidden/index';
 export * from './generic-form/index';
 export * from './navigation/index';
