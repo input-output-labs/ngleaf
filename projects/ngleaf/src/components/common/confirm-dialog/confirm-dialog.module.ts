098aca1f8e3e57ff7c5c3732a57f16cb7918b24e
NablaT <remi.pourtier@gmail.com>
2024-02-28 14:33:38 +0100

    v2.9.34: adding loc for confirmation dialog

--- projects/ngleaf/src/components/common/confirm-dialog/confirm-dialog.module.ts
+++ projects/ngleaf/src/components/common/confirm-dialog/confirm-dialog.module.ts
@@ -1,21 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatButtonModule } from "@angular/material/button";
 
-
-import { LeafConfirmDialogComponent } from './confirm-dialog.component';
+import { LeafConfirmDialogComponent } from "./confirm-dialog.component";
+import { TranslateModule } from "@ngx-translate/core";
 
 @NgModule({
   declarations: [LeafConfirmDialogComponent],
   imports: [
     /* Code deps */
     CommonModule,
+    TranslateModule,
     /* Material deps */
     MatDialogModule,
     MatButtonModule,
     /* Leaf deps*/
   ],
-  exports: [LeafConfirmDialogComponent]
+  exports: [LeafConfirmDialogComponent],
 })
 export class LeafConfirmDialogModule {}
