1bc65c93d1b817d7f5edcd798349233fd822357f
NablaT <remi.pourtier@gmail.com>
2024-12-14 15:22:14 +0100

    2.18.2: adding new endpoint to check if email exists

--- projects/ngleaf/src/api/clients/session-api-client/account-api-client.service.ts
+++ projects/ngleaf/src/api/clients/session-api-client/account-api-client.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 
 import { LeafAuthHttpClient } from '../auth-http-client/leaf-auth-http-client.service';
 import { JWTModel, LeafAccountModel, LeafAccountProfile, LoginModel, PasswordChangingModel, PasswordResettingModel, RegistrationModel } from '../../models/index';
@@ -14,7 +14,7 @@ export class AccountApiClient {
   public constructor(
     @Inject(LeafApiClientConfigServiceToken) public config: LeafApiClientConfig,
     public authHttp: LeafAuthHttpClient
-    ) {}
+  ) {}
 
   public setup(setupConfig: LeafSetupConfig): Observable<LeafSetupResponse> {
     return this.authHttp.get<LeafSetupResponse>(
@@ -23,50 +23,100 @@ export class AccountApiClient {
   }
 
   public me(): Observable<LeafAccountModel> {
-    return this.authHttp.get<LeafAccountModel>(this.config.serverUrl + '/account/me');
+    return this.authHttp.get<LeafAccountModel>(
+      this.config.serverUrl + "/account/me"
+    );
   }
 
   public register(registration: RegistrationModel): Observable<JWTModel> {
-    return this.authHttp.post<JWTModel>(this.config.serverUrl + '/account', registration);
+    return this.authHttp.post<JWTModel>(
+      this.config.serverUrl + "/account",
+      registration
+    );
   }
 
   public login(login: LoginModel): Observable<JWTModel> {
-    return this.authHttp.post<JWTModel>(this.config.serverUrl + '/account/login', login);
+    return this.authHttp.post<JWTModel>(
+      this.config.serverUrl + "/account/login",
+      login
+    );
   }
 
-  public updateProfile(updates: LeafAccountProfile): Observable<LeafAccountModel> {
-    return this.authHttp.post<LeafAccountModel>(this.config.serverUrl + '/account/me/profile', updates);
+  public updateProfile(
+    updates: LeafAccountProfile
+  ): Observable<LeafAccountModel> {
+    return this.authHttp.post<LeafAccountModel>(
+      this.config.serverUrl + "/account/me/profile",
+      updates
+    );
   }
 
   public changeUsername(newUsername: string): Observable<LeafAccountModel> {
-    return this.authHttp.post<LeafAccountModel>(this.config.serverUrl + '/account/me/username', newUsername);
+    return this.authHttp.post<LeafAccountModel>(
+      this.config.serverUrl + "/account/me/username",
+      newUsername
+    );
   }
 
-  public changePassword(passwordChange: PasswordChangingModel): Observable<LeafAccountModel> {
-    return this.authHttp.post<LeafAccountModel>(this.config.serverUrl + '/account/me/password', passwordChange);
+  public changePassword(
+    passwordChange: PasswordChangingModel
+  ): Observable<LeafAccountModel> {
+    return this.authHttp.post<LeafAccountModel>(
+      this.config.serverUrl + "/account/me/password",
+      passwordChange
+    );
   }
 
   public sendPasswordKey(emailOfAccountToReset: string): Observable<void> {
-    return this.authHttp.post<void>(this.config.serverUrl + '/account/sendresetpasswordkey', emailOfAccountToReset);
+    return this.authHttp.post<void>(
+      this.config.serverUrl + "/account/sendresetpasswordkey",
+      emailOfAccountToReset
+    );
   }
 
-  public resetPassword(passwordResetting: PasswordResettingModel): Observable<void> {
-    return this.authHttp.post<void>(this.config.serverUrl + '/account/resetPassword', passwordResetting);
+  public resetPassword(
+    passwordResetting: PasswordResettingModel
+  ): Observable<void> {
+    return this.authHttp.post<void>(
+      this.config.serverUrl + "/account/resetPassword",
+      passwordResetting
+    );
   }
 
   public changeAvatar(newAvatarUrl: string): Observable<LeafAccountModel> {
-    return this.authHttp.post<LeafAccountModel>(this.config.serverUrl + '/account/me/avatar', newAvatarUrl);
+    return this.authHttp.post<LeafAccountModel>(
+      this.config.serverUrl + "/account/me/avatar",
+      newAvatarUrl
+    );
   }
 
   public autocomplete(input: string): Observable<LeafAccountModel[]> {
-      const params = new HttpParams().set('input', input);
-      return this.authHttp.get<LeafAccountModel[]>(
-          this.config.serverUrl + '/account/autocomplete',
-          { params }
-      );
+    const params = new HttpParams().set("input", input);
+    return this.authHttp.get<LeafAccountModel[]>(
+      this.config.serverUrl + "/account/autocomplete",
+      { params }
+    );
   }
 
-  public unsubscribeFromEmail(unsubscription: { email: string; type: string; }) {
-    return this.authHttp.post<void>(this.config.serverUrl + '/account/mailings/unsubscription', unsubscription);
+  public unsubscribeFromEmail(unsubscription: { email: string; type: string }) {
+    return this.authHttp.post<void>(
+      this.config.serverUrl + "/account/mailings/unsubscription",
+      unsubscription
+    );
+  }
+
+  public checkIfEmailExists(email: string): Observable<boolean> {
+    const params = new HttpParams().set("email", email);
+    return this.authHttp
+      .get<void>(`${this.config.serverUrl}/account/email`, { params })
+      .pipe(
+        map(() => true),
+        catchError((error) => {
+          if (error.status === 404) {
+            return of(false);
+          }
+          throw error;
+        })
+      );
   }
 }
